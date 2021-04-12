8ca6edad53155c757d20001a67397b85de517de6
Alexey Germanovich <alexei.germanovich@yandex.ru>
2021-04-13 00:52:49 +0300

    feat: implement function encodeLine

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let counter = 1;
+  let result = '';
+
+  if (str.length === 1) {
+    return str;
+  }
+
+  str.split('').forEach((item, index, arr) => {
+    if (item === arr[index + 1]) {
+      counter++;
+    } else {
+      const count = counter === 1 ? '' : counter;
+      result += `${count}${item}`;
+      counter = 1;
+    }
+  });
+  return result;
 }
 
 module.exports = encodeLine;
